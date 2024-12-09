fa9f644baf94893c04c425de72e02bc4bb7b2f7e
AFDtea <77896483+AFDtea@users.noreply.github.com>
2024-12-09 10:15:08 -0500

    Update vector.store.ts

--- src/modules/agent/vector.store.ts
+++ src/modules/agent/vector.store.ts
@@ -27,10 +27,7 @@ export default async function initVectorStore(
           id: node.id,
           title: node.title,
           abstract: node.abstract,
-          category: node.category,
-          authors: [ (person)-[:AUTHORED]->(node) | person.name ],
-          citations: [ (citing)-[:CITES]->(node) | citing.title ],
-          references: [ (node)-[:CITES]->(cited) | cited.title ]
+          category: node.category
         } AS metadata
     `,
    })
